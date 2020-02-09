f55388080aab3112ea73f3b96353c54a298823ec
UbuntuEckart <mhme2000@hotmail.com>
2020-02-09 20:11:15 -0200

    Create first controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() {
+        $products = ['Product 01', 'Product 02', 'Product 03'];
+
+        return $products;
+    }
+
+    public function show($id) {
+        return "Exibindo o produto de id {$id}";
+    }
+
+    public function create() {
+        return "Formulário para criação de novos produtos";
+    }
+}
